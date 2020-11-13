23b93172879b0a27c60c53130e0f88256299c96c
MaksymKravchenkoGM <mak@global-mediator.com>
2020-11-13 11:09:16 +0200

    Change error message

--- web/viewer.js
+++ web/viewer.js
@@ -912,7 +912,7 @@ const PDFViewerApplication = {
       let loadingErrorMessage;
 
       if (exception instanceof _pdfjsLib.InvalidPDFException) {
-        loadingErrorMessage = this.l10n.get("invalid_file_error", null, "Invalid or corrupted PDF file.");
+        loadingErrorMessage = this.l10n.get("invalid_file_error", null, "Document does not contain a PDF file.");
       } else if (exception instanceof _pdfjsLib.MissingPDFException) {
         loadingErrorMessage = this.l10n.get("missing_file_error", null, "Missing PDF file.");
       } else if (exception instanceof _pdfjsLib.UnexpectedResponseException) {
