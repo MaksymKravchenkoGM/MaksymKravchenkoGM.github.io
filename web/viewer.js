604067922623eb3e83b93b294ac8d587a21230ca
MaksymKravchenkoGM <mak@global-mediator.com>
2020-12-15 16:39:11 +0200

    Revert "test file name"
    This reverts commit fd4b9e2a66aacaa891feded080d3e3e07c7829dc.

--- web/viewer.js
+++ web/viewer.js
@@ -3207,39 +3207,33 @@ function isDataSchema(url) {
 
   return url.substring(i, i + 5).toLowerCase() === "data:";
 }
-var pdffilenameBC;
-function LoadFileName(filename) {
-
- pdffilenameBC = filename;
-}
 
 function getPDFFileNameFromURL(url, defaultFilename = "document.pdf") {
-return pdffilenameBC;
-  // if (typeof url !== "string") {
-  //   return defaultFilename;
-  // }
+  if (typeof url !== "string") {
+    return defaultFilename;
+  }
 
-  // if (isDataSchema(url)) {
-  //   console.warn("getPDFFileNameFromURL: " + 'ignoring "data:" URL for performance reasons.');
-  //   return defaultFilename;
-  // }
+  if (isDataSchema(url)) {
+    console.warn("getPDFFileNameFromURL: " + 'ignoring "data:" URL for performance reasons.');
+    return defaultFilename;
+  }
 
-  // const reURI = /^(?:(?:[^:]+:)?\/\/[^\/]+)?([^?#]*)(\?[^#]*)?(#.*)?$/;
-  // const reFilename = /[^\/?#=]+\.pdf\b(?!.*\.pdf\b)/i;
-  // const splitURI = reURI.exec(url);
-  // let suggestedFilename = reFilename.exec(splitURI[1]) || reFilename.exec(splitURI[2]) || reFilename.exec(splitURI[3]);
+  const reURI = /^(?:(?:[^:]+:)?\/\/[^\/]+)?([^?#]*)(\?[^#]*)?(#.*)?$/;
+  const reFilename = /[^\/?#=]+\.pdf\b(?!.*\.pdf\b)/i;
+  const splitURI = reURI.exec(url);
+  let suggestedFilename = reFilename.exec(splitURI[1]) || reFilename.exec(splitURI[2]) || reFilename.exec(splitURI[3]);
 
-  // if (suggestedFilename) {
-  //   suggestedFilename = suggestedFilename[0];
+  if (suggestedFilename) {
+    suggestedFilename = suggestedFilename[0];
 
-  //   if (suggestedFilename.includes("%")) {
-  //     try {
-  //       suggestedFilename = reFilename.exec(decodeURIComponent(suggestedFilename))[0];
-  //     } catch (ex) {}
-  //   }
-  // }
+    if (suggestedFilename.includes("%")) {
+      try {
+        suggestedFilename = reFilename.exec(decodeURIComponent(suggestedFilename))[0];
+      } catch (ex) {}
+    }
+  }
 
-  // return suggestedFilename || defaultFilename;
+  return suggestedFilename || defaultFilename;
 }
 
 function normalizeWheelEventDelta(evt) {
